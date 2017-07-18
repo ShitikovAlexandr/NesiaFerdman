7278b424e9ea4d02c60b30bdd530f03f8268ab64
Alex_Shitikov <shitikov.net@gmail.com>
2017-07-18 18:33:53 +0300

    Add calendar list data source

---
+++ NesiaFerdman/Controllers/CalendarListController/NFCalendarListDataSource.h
@@ -0,0 +1,13 @@
+//
+//  NFCalendarListDataSource.h
+//  NesiaFerdman
+//
+//  Created by Alex_Shitikov on 7/18/17.
+//  Copyright © 2017 Gemicle. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NFCalendarListDataSource : NSObject
+
+@end
---
+++ NesiaFerdman/Controllers/CalendarListController/NFCalendarListDataSource.m
@@ -0,0 +1,13 @@
+//
+//  NFCalendarListDataSource.m
+//  NesiaFerdman
+//
+//  Created by Alex_Shitikov on 7/18/17.
+//  Copyright © 2017 Gemicle. All rights reserved.
+//
+
+#import "NFCalendarListDataSource.h"
+
+@implementation NFCalendarListDataSource
+
+@end
