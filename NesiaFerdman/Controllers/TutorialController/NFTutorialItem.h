9e93ccb6b0185b8c8f8f0f1e1a766dc07bbbacc5
Alex_Shitikov <shitikov.net@gmail.com>
2017-09-15 16:13:52 +0300

    Add tutorial model

---
+++ NesiaFerdman/Controllers/TutorialController/NFTutorialItem.h
@@ -0,0 +1,17 @@
+//
+//  NFTutorialItem.h
+//  NesiaFerdman
+//
+//  Created by Alex_Shitikov on 9/15/17.
+//  Copyright © 2017 Gemicle. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NFTutorialItem : NSObject
+
+@property (strong, nonatomic) NSString *title;
+@property (strong, nonatomic) NSString *text;
+@property (strong, nonatomic) NSString *imageName;
+
+@end
---
+++ NesiaFerdman/Controllers/TutorialController/NFTutorialItem.m
@@ -0,0 +1,13 @@
+//
+//  NFTutorialItem.m
+//  NesiaFerdman
+//
+//  Created by Alex_Shitikov on 9/15/17.
+//  Copyright © 2017 Gemicle. All rights reserved.
+//
+
+#import "NFTutorialItem.h"
+
+@implementation NFTutorialItem
+
+@end
