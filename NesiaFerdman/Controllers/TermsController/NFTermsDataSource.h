0300620a5499390e64b1bf7fca8948226cb434ae
Alex_Shitikov <shitikov.net@gmail.com>
2017-09-25 18:01:52 +0300

    add data source for terms controller

---
+++ NesiaFerdman/Controllers/TermsController/NFTermsDataSource.h
@@ -0,0 +1,15 @@
+//
+//  NFTermsDataSource.h
+//  NesiaFerdman
+//
+//  Created by Alex_Shitikov on 9/25/17.
+//  Copyright © 2017 Gemicle. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface NFTermsDataSource : NSObject
+@property (strong, nonatomic) NSString *mainInfo;
+
+@end
